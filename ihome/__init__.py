545104e77747302d68ef86a91444409e79ca14ff
xuecuiping <xuecuiping78@163.com>
2020-03-18 23:40:02 +0800

    3月18

--- ihome/__init__.py
+++ ihome/__init__.py
@@ -1,17 +1,36 @@
 #coding:utf-8
+import redis
+import logging
+
+# from config import config_dict
 from flask import Flask
 from config import config_map
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import CSRFProtect
+from flask_wtf.csrf import CSRFProtect
 from flask_session import Session
-from ihome import api_1_0
+from logging.handlers import RotatingFileHandler
 
-import redis
-# 数据库
+# 构建数据库对象
 db=SQLAlchemy()
 
 # 创建redis连接对象
 redis_store=None
+
+# # 为了flask补充csrf防护机制
+# csrf=CSRFProtect()
+
+
+
+# 创建日志记录器，指明日志保存的路径，每个日志文件的最大大小，保存的日志文件个数上限
+file_log_handler=RotatingFileHandler("logs/log",maxBytes=1024*1024*100,backupCount=10)
+# 创建日志记录的格式            日志扥飞机，，输入日志信息的文件名，，行数，，，日志信息
+formatter=logging.Formatter("%(levelname)s %(filename)s:%(lineno)d %(message)s")
+# 为刚创建的日志记录器设置日志记录格式
+file_log_handler.setFormatter(formatter)
+# 为全局的日志工具对象  （flask  app使用的）  添加日志记录器
+logging.getLogger().addHandler(file_log_handler)
+# 设置日志的记录等级
+logging.basicConfig(level=logging.DEBUG)#调试debug等级
 
 
 # 工厂模式
@@ -21,7 +40,13 @@ def create_app(config_name):
     :param config_name:str 配置模式的模式的名字（"develop","product"）
     :return:
     """
+
     app = Flask(__name__)
+
+    # conf=config_dict[config_name]
+    # # 设置flask的配置信息
+    # app.config.from_object(conf)
+
     # 根据配置模式的名字获取配置参数的类
     config_class=config_map.get(config_name)
     app.config.from_object(config_class)
@@ -33,6 +58,9 @@ def create_app(config_name):
     global redis_store
     redis_store = redis.StrictRedis(host=config_class.REDIS_HOST, port=config_class.REDIS_PORT)
 
+    # # 初始化
+    # csrf.init_app(app)
+
     # 利用flask-session,将session数据保存到redis中
     Session(app)
 
@@ -40,6 +68,7 @@ def create_app(config_name):
     CSRFProtect(app)
 
     # 注册蓝图
+    from ihome import api_1_0
     app.register_blueprint(api_1_0.api,url_prefix='/api/v1.0')
 
     return app
